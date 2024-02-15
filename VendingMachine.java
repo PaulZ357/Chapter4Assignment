6c46ea3dc96791727bbf82dc8e7bd600781ccebb
cakeller2 <cakeller2@quinnipiac.edu>
2024-02-15 15:36:08 -0500

    code prints out 10 different snacks and their prices

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class VendingMachine {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //sets up scanner
+
+        System.out.println("The options are:\n1. Snickers $1.50\n2. Poptarts $1.25\n3. Sun Chips $2.00\n4. Reese's Peanut Butter Cups  $1.50");
+        System.out.println("5. Trail Mix $3.00\n6. Granola Bars $2.50\n7. Cheez-its $2.00\n8. Pretzels $2.00\n9. Chex Mix $2.25\n10. Sour Patch Kids $3.00");
         
     }
 }
