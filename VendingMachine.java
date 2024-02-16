9a9faee027d3b6cfcd2ecdda01824fcee2d35248
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-16 10:38:58 -0500

    reorganized code for better efficiency

--- VendingMachine.java
+++ VendingMachine.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 // Creating a snack class
 class Snacks {
     // Declaring and initializing the attributes
-    String name;
-    String price;
+    private String name;
+    private String price;
 
     Snacks(String name, String price) {
         this.name = name;
         this.price = price;
     }
 
+    String getName() {
+        return this.name;
+    }
+
+    String getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.price;
+    }
   }
 
 public class VendingMachine {
@@ -32,15 +44,16 @@ public class VendingMachine {
         snackCollect[9] = new Snacks("Sour Patch Kids", "$3.50");
 
         System.out.println("Hello! Please pick the snack you want from the vending machine.");
-        System.out.println("The options are:\n1. " + snackCollect[0].name + " " + snackCollect[0].price + "\n2. " + snackCollect[1].name + " " + snackCollect[1].price + "\n3. " + snackCollect[2].name + " " + snackCollect[2].price + "\n4. " + snackCollect[3].name + " " + snackCollect[3].price);
-        System.out.println("5. " + snackCollect[4].name + " " + snackCollect[4].price + "\n6. " + snackCollect[5].name + " " + snackCollect[5].price + "\n7. " + snackCollect[6].name + " " + snackCollect[6].price + "\n8. " + snackCollect[7].name + " " + snackCollect[7].price);
-        System.out.println("9. " + snackCollect[8].name + " " + snackCollect[8].price + "\n10. " + snackCollect[9].name + " " + snackCollect[9].price);
+        System.out.println("The options are:");
+        for (int i=1; i<=10; i++) {
+            System.out.println(i+". " + snackCollect[i-1].toString());
+        }
         
         // Prompts the user to purchase an item.
         System.out.print("Enter the number of the item you want to order: ");
         int selection = input.nextInt();
 
-        System.out.println("You selected " + snackCollect[selection-1].name + " which costs " + snackCollect[selection-1].price);
+        System.out.println("You selected " + snackCollect[selection-1].getName() + " which costs " + snackCollect[selection-1].getPrice());
         System.out.println("How much money would you like to put into the machine? This machine only takes $1");
         int bills = input.nextInt();
 
