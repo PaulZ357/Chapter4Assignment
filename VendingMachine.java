b91e2488ca340b9c49219dc926382f6f3344e5e6
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-16 10:57:05 -0500

    Added code for paying bills

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 // Creating a snack class
-class Snacks {
+class Snack {
     // Declaring and initializing the attributes
     private String name;
-    private String price;
+    private double price;
 
-    Snacks(String name, String price) {
+    Snack(String name, double price) {
         this.name = name;
         this.price = price;
     }
@@ -15,13 +15,13 @@ class Snacks {
         return this.name;
     }
 
-    String getPrice() {
+    double getPrice() {
         return this.price;
     }
 
     @Override
     public String toString() {
-        return this.name + " " + this.price;
+        return this.name + " $" + this.price;
     }
   }
 
@@ -29,19 +29,19 @@ public class VendingMachine {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //sets up scanner
 
-        Snacks[] snackCollect = new Snacks[10];
+        Snack[] snackCollect = new Snack[10];
 
         //creating all the snack objects
-        snackCollect[0] = new Snacks("Snickers", "$1.50");
-        snackCollect[1] = new Snacks("Poptarts", "$1.25");
-        snackCollect[2] = new Snacks("Sun Chips", "$2.00");
-        snackCollect[3] = new Snacks("Reese's Peanut Butter Cups", "$1.50");
-        snackCollect[4] = new Snacks("Trail Mix", "$3.00");
-        snackCollect[5] = new Snacks("Granola Bars", "$2.50");
-        snackCollect[6] = new Snacks("Cheez-its", "$2.00");
-        snackCollect[7] = new Snacks("Pretzels", "$2.50");
-        snackCollect[8] = new Snacks("Chex Mix", "$2.50");
-        snackCollect[9] = new Snacks("Sour Patch Kids", "$3.50");
+        snackCollect[0] = new Snack("Snickers", 1.50); //$1.50
+        snackCollect[1] = new Snack("Poptarts", 1.25); //$1.25
+        snackCollect[2] = new Snack("Sun Chips", 2.00); //$2.00
+        snackCollect[3] = new Snack("Reese's Peanut Butter Cups", 1.50); //$1.50
+        snackCollect[4] = new Snack("Trail Mix", 3.00); //$3.00
+        snackCollect[5] = new Snack("Granola Bars", 2.50); //$2.50
+        snackCollect[6] = new Snack("Cheez-its", 2.00); //$2.00
+        snackCollect[7] = new Snack("Pretzels", 2.50); //$2.50
+        snackCollect[8] = new Snack("Chex Mix", 2.50); //$2.50
+        snackCollect[9] = new Snack("Sour Patch Kids", 3.50); //$3.50
 
         System.out.println("Hello! Please pick the snack you want from the vending machine.");
         System.out.println("The options are:");
@@ -53,9 +53,24 @@ public class VendingMachine {
         System.out.print("Enter the number of the item you want to order: ");
         int selection = input.nextInt();
 
-        System.out.println("You selected " + snackCollect[selection-1].getName() + " which costs " + snackCollect[selection-1].getPrice());
-        System.out.println("How much money would you like to put into the machine? This machine only takes $1");
+        Snack chosen = snackCollect[selection-1];
+        double price = chosen.getPrice();
+        System.out.println();
+        System.out.println("You selected " + chosen.getName() + " which costs $" + price);
+        System.out.print("How much money would you like to put? We can only take $1 bills: ");
         int bills = input.nextInt();
 
+        double change = bills-price;
+        if (change < 0) {
+            System.out.println("Insufficient cash.");
+        } else {
+            if (change > 0) {
+                System.out.println("Your change is $"+change);
+            } else {
+                System.out.println("You currently have no change");
+                
+            }
+            System.out.println("Thank you and have a nice day :)");
+        }
     }
 }
